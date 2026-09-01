3910b564d8b6030c7009e5dd0551efd986e36d48
Sara-Pesso <s.pessognelli@outlook.com>
2026-08-31 20:52:43 -0400

    fix hamiltonian py

--- hamiltonian_cycle/dp.py
+++ hamiltonian_cycle/dp.py
@@ -1,3 +1,4 @@
+from math import nan
 def has_hamiltonian_cycle(adj_matrix):
     # number of nodes
     n = len(adj_matrix)
@@ -15,60 +16,35 @@ def has_hamiltonian_cycle(adj_matrix):
     
     # Iterate through all masks (subsets of nodes)
     for mask in range(num_states):
-        # # Make sure we are starting at node 0 (node 0 = bin(1) = 0001)
-        # if not (mask & 1):
-        #     continue
-            
         for i in range(n):
-            # If current state is unreachable, move on
+            # If the current state is unreachable, skip it
             if dp[mask][i] == -1:
                 continue
+            
+            # Make sure node i is part of the current mask
+            if mask & (1 << i):
 
-            # Look for a preceding node j
-            for j in range(n):
+                # Look for a next node j
+                for j in range(n):
 
-                # Check if edge exists and node j is not yet visited in this mask
-                if adj_matrix[i][j] == 1 and not (mask & (1 << j)):
-                    next_mask = mask | (1 << j)
-                    dp[next_mask][j] = i
+                    # j must not be in the mask, distinct from i, and connected to i
+                    if j != i and not (mask & (1 << j)) and adj_matrix[i][j]:
+                        
 
-    # Check if a complete cycle exists back to vertex 0
-    final_mask = (1 << n) - 1
-    end_vertex = -1
+                        # If a valid path ended at j -> i
+                        next_mask = mask | (1 << j)
+                        dp[next_mask][j] = i
 
-    for u in range(n):
-        # Must visit all nodes and have a valid edge back to 0
-        if dp[final_mask][u] != -1 and adj_matrix[u][0] == 1:
-            end_vertex = u
-            break
+    # Check if we can complete the cycle back to node 0 from any ending node i
+    full_mask = num_states - 1
 
-    # If no end vertex found, no Hamiltonian Cycle exists
-    if end_vertex == -1:
-        print("No Hamiltonian Cycle exists in this graph.")
-        return None
+    for i in range(n):
+        if dp[full_mask][i] and adj_matrix[i][0]:
+            return True, dp
+            
+    return False, dp
 
-    # --- Reconstruction Path ---
-    path = []
-    curr_vertex = end_vertex
-    curr_mask = final_mask
-
-    print(curr_vertex, curr_mask)
-
-    while curr_mask > 0:
-        path.append(curr_vertex)
-        prev_vertex = dp[curr_mask][curr_vertex]
-        curr_mask = curr_mask ^ (1 << curr_vertex) # Remove current vertex from mask
-        curr_vertex = prev_vertex
-
-    path.reverse()
-    path.append(0)  # Complete the cycle by returning to the start node
-
-    print("Hamiltonian Cycle found:")
-    print(" -> ".join(map(str, path)))
-    return path
-
-
-example_graph = [
+adj = [
     [0, 1, 0, 1, 1],
     [1, 0, 1, 1, 0],
     [0, 1, 0, 1, 1],
@@ -76,4 +52,37 @@ example_graph = [
     [1, 0, 1, 1, 0]
 ]
 
-has_hamiltonian_cycle(example_graph)
+ham_check, dp = has_hamiltonian_cycle(adj)
+
+# --- Construct Hamiltonian Cycle from bitmasks ---
+if ham_check: # i.e., there exists a hamiltonian cycle somewhere in this graph
+    n = len(adj)
+    final_mask = (1 << n) - 1
+    end_node = -1
+    for i in range(n):
+        # Needs to visit all nodes and connect to node 0
+        if dp[final_mask][i] != -1 and adj[i][0] == 1:
+            end_node = i
+            break
+
+    if end_node == -1: 
+        print("No Hamiltonian Cycle")
+
+    else:
+        path = []
+        curr_vertex = end_node
+        curr_mask = final_mask
+
+        while curr_mask > 0:
+            path.append(curr_vertex)
+            print(curr_vertex)
+            prev_vertex = dp[curr_mask][curr_vertex]
+            curr_mask = curr_mask ^ (1 << curr_vertex) # Remove current vertex from mask
+            curr_vertex = prev_vertex
+
+        path.reverse()
+        path.append(0)  # Complete the cycle by returning to the start node
+
+        print("Hamiltonian Cycle found:")
+        print(" -> ".join(map(str, path)))
+        # return path
